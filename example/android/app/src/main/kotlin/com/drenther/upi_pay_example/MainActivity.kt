7b527cd5de8a4e87a260d9e0a02c78df1ababef6
prashanthraj24 <prashanthraj@asthatrade.com>
2022-04-04 22:39:47 +0530

    update 1

--- example/android/app/src/main/kotlin/com/drenther/upi_pay_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/drenther/upi_pay_example/MainActivity.kt
@@ -1,11 +1,17 @@
+// package com.drenther.upi_pay_example
+
+// import androidx.annotation.NonNull
+// import io.flutter.embedding.android.FlutterFragmentActivity
+// import io.flutter.embedding.engine.FlutterEngine
+// import io.flutter.plugins.GeneratedPluginRegistrant
+// class MainActivity: FlutterFragmentActivity() {
+//     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//         GeneratedPluginRegistrant.registerGeneratedPlugins(FlutterEngine(this));
+//     }
+// }
 package com.drenther.upi_pay_example
 
-import androidx.annotation.NonNull
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
-class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegister.registerGeneratedPlugins(FlutterEngine(this));
-    }
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
 }
